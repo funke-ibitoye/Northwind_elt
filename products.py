1dec04971ea0f0323be4224a4c31d492b2979644
funke-ibitoye <144049357+funke-ibitoye@users.noreply.github.com>
2024-01-21 20:21:27 +0100

    Add files via upload

---
+++ products.py
@@ -0,0 +1,21 @@
+import requests
+import pandas as pd
+import configparser
+from sqlalchemy import create_engine
+
+config=configparser.ConfigParser()
+config.read('config.ini')
+
+postgres_config=config['postgres']
+engine=create_engine(
+    f"postgresql://{postgres_config['user']}:{postgres_config['password']}@{postgres_config['host']}/{postgres_config['database']}"
+)
+
+url=f'https://demodata.grapecity.com/northwind/api/v1/Products'
+response=requests.get(url)
+data=response.json()
+print(data)
+
+df=pd.json_normalize(data)
+df.to_sql('products_raw',engine,if_exists='replace',index=False)
+engine.dispose()
